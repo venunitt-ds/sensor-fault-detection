67e72b1aa38148d8ccea88af96df304242598bf2
venunitt-ds <114333024+venunitt-ds@users.noreply.github.com>
2022-10-27 19:44:06 +0530

    password change

--- sensor/configuration/mongo_db_connection.py
+++ sensor/configuration/mongo_db_connection.py
@@ -8,7 +8,7 @@ class MongoDBClient:
     def __init__(self, database_name=DATABASE_NAME) -> None:
         try:
             if MongoDBClient.client is None:
-                mongo_db_url = "mongodb+srv://venuplla:venu2023@ineuron.8x1bgv4.mongodb.net/?retryWrites=true&w=majority"
+                mongo_db_url = "mongodb+srv://venuplla:<passsword>@ineuron.8x1bgv4.mongodb.net/?retryWrites=true&w=majority"
                 MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
             self.client = MongoDBClient.client
             self.database = self.client[database_name]
